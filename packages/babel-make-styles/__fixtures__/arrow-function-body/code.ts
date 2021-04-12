dab0cdada5756b08c062771c22ef4247463abf86
Oleksandr Fediashov <olfedias@microsoft.com>
2021-04-12 15:44:14 +0200

    wip

--- packages/babel-make-styles/__fixtures__/arrow-function-body/code.ts
+++ packages/babel-make-styles/__fixtures__/arrow-function-body/code.ts
@@ -1,6 +1,7 @@
 import { makeStyles } from '@fluentui/react-make-styles';
+import { Theme } from '@fluentui/react-theme';
 
-function buttonTokens(theme) {
+function buttonTokens(theme: Theme) {
   return {
     backgroundColor: theme.global.color.black,
     backgroundColorHover: 'red',
